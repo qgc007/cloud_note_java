8a6b38dccf922d434e1ea9880785d37d6bf406b0
shinn <qinguoce@126.com>
2022-07-19 01:47:17 +0900

    project:cloud node

---
+++ src/main/java/com/kebab/cloud_note_java/service/TbUserService.java
@@ -0,0 +1,13 @@
+package com.kebab.cloud_note_java.service;
+
+import com.kebab.cloud_note_java.pojo.TbUser;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author shin
+* @description 针对表【tb_user】的数据库操作Service
+* @createDate 2022-07-19 01:04:41
+*/
+public interface TbUserService extends IService<TbUser> {
+
+}
