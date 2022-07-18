4373bfd654839f8acc081dbcce112a3577523c1c
shinn <qinguoce@126.com>
2022-07-19 01:46:51 +0900

    project:cloud node

---
+++ src/main/java/com/kebab/cloud_note_java/mapper/TbUserMapper.java
@@ -0,0 +1,18 @@
+package com.kebab.cloud_note_java.mapper;
+
+import com.kebab.cloud_note_java.pojo.TbUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author shin
+* @description 针对表【tb_user】的数据库操作Mapper
+* @createDate 2022-07-19 01:04:41
+* @Entity com.kebab.cloud_note_java.pojo.TbUser
+*/
+public interface TbUserMapper extends BaseMapper<TbUser> {
+
+}
+
+
+
+
