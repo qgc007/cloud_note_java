6bef8280665ea1f10342e187a806cf6aa70a8e8d
shinn <qinguoce@126.com>
2022-07-19 01:48:37 +0900

    project:cloud node

--- src/main/java/com/kebab/cloud_note_java/CloudNoteJavaApplication.java
+++ src/main/java/com/kebab/cloud_note_java/CloudNoteJavaApplication.java
@@ -1,9 +1,11 @@
 package com.kebab.cloud_note_java;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.kebab.cloud_note_java.mapper")
 public class CloudNoteJavaApplication {
 
     public static void main(String[] args) {
