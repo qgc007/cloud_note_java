6e539440723cde420b05301489570f3507602bf0
shinn <qinguoce@126.com>
2022-07-23 20:54:10 +0900

    project:cloud node

--- src/main/java/com/kebab/cloud_note_java/controller/LoginController.java
+++ src/main/java/com/kebab/cloud_note_java/controller/LoginController.java
@@ -2,8 +2,11 @@ package com.kebab.cloud_note_java.controller;
 import com.kebab.cloud_note_java.pojo.Result;
 import com.kebab.cloud_note_java.pojo.TbUser;
 import com.kebab.cloud_note_java.service.impl.TbUserServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +18,17 @@ public class LoginController {
     @Autowired
     TbUserServiceImpl tbUserServiceImpl;
 
+    //日志
+    private final static Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     /**
      * 登录
      * @return
      */
     @RequestMapping("/sign_in")
-    public  Result userSignIn(@RequestBody(required=true)TbUser user){
+    public  Result userSignIn(@Validated @RequestBody(required=true)TbUser user){
         System.out.println("---------------------login start----------------");
+        logger.info("==========userSignIn Start==========");
         System.out.println(user.getUMail());
         TbUser tbUserParam = user;
         //String md5Str = DigestUtils.md5DigestAsHex(tbUserParam.getUPwd().getBytes());
@@ -29,6 +36,7 @@ public class LoginController {
         System.out.println(tbUser.getUName());
         System.out.println("---------------------login end----------------");
         return  new Result(Result.Code.OK, null,"登录成功");
+        //return Results
     }
 
     /**
@@ -36,7 +44,7 @@ public class LoginController {
      * @return
      */
     @RequestMapping("/sign_up")
-    public Result userSignUp(@RequestBody(required=true)TbUser user){
+    public Result userSignUp(@Validated  @RequestBody(required=true)TbUser user){
         TbUser tbUser = user;
         //密码加密 MD5
         String md5Str = DigestUtils.md5DigestAsHex(user.getUPwd().getBytes());
